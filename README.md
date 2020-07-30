889f2f8ff7d1471bbdbe3ede26cb5c1d1d69e397
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2020-07-30 16:00:22 +0700

    change some status codes in HTTP response

--- README.md
+++ README.md
@@ -3,6 +3,24 @@
 For more detail, please visit:
 > [Spring Boot with MongoDB CRUD example using Spring Data](https://bezkoder.com/spring-boot-mongodb-crud/)
 
+More Practice:
+> [Spring Boot MongoDB Pagination & Filter example](https://bezkoder.com/spring-boot-mongodb-pagination/)
+
+Exception Handling:
+> [Spring Boot @ControllerAdvice & @ExceptionHandler example](https://bezkoder.com/spring-boot-controlleradvice-exceptionhandler/)
+
+> [@RestControllerAdvice example in Spring Boot](https://bezkoder.com/spring-boot-restcontrolleradvice/)
+
+Security:
+> [Spring Boot: JWT Authentication & Authorization with MongoDB](https://bezkoder.com/spring-boot-jwt-auth-mongodb/)
+
+Fullstack:
+> [Angular 10 + Spring Boot + MongoDB](https://bezkoder.com/angular-10-spring-boot-mongodb/)
+
+> [Vue.js + Spring Boot + MongoDB](https://bezkoder.com/spring-boot-vue-mongodb/)
+
+> [React + Spring Boot + MongoDB](https://bezkoder.com/react-spring-boot-mongodb/)
+
 ## Run Spring Boot application
 ```
 mvn spring-boot:run
--- src/main/java/com/bezkoder/spring/data/mongodb/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/data/mongodb/controller/TutorialController.java
@@ -66,7 +66,7 @@ public class TutorialController {
       Tutorial _tutorial = tutorialRepository.save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
       return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
     } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.EXPECTATION_FAILED);
+      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
@@ -91,7 +91,7 @@ public class TutorialController {
       tutorialRepository.deleteById(id);
       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
@@ -101,7 +101,7 @@ public class TutorialController {
       tutorialRepository.deleteAll();
       return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
@@ -115,7 +115,7 @@ public class TutorialController {
       }
       return new ResponseEntity<>(tutorials, HttpStatus.OK);
     } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
